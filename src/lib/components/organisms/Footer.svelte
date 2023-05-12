9828c57fbebe053d5f2892f91d64681a86bcc917
Jorge Lewis <antopiahk@gmail.com>
2023-05-12 13:05:28 +0800

    social medias on footer

--- src/lib/components/organisms/Footer.svelte
+++ src/lib/components/organisms/Footer.svelte
@@ -1,34 +1,50 @@
 <script lang="ts">
 	import Logo from '$lib/components/atoms/Logo.svelte';
+	import Icon from '../atoms/Icon.svelte';
 </script>
 
 <footer
-	class="{$$props.class} pt-20 p-6 border-t border-secondary-light/10 dark:border-secondary-dark/10"
+	class="{$$props.class} pt-12 p-6 border-t border-secondary-light/10 dark:border-secondary-dark/10"
 >
 	<div
 		class="sm:flex sm:justify-between sm:items-center md:px-10 lg:px-20 xl:px-40 px-auto md:px-auto"
 	>
 		<Logo />
 		<ul
-			class="flex flex-wrap items-center mb-6 space-x-6 text-sm  sm:mb-0 text-background-on-light dark:text-background-on-dark"
+			class="grid grid-cols-4 place-items-center mb-6 space-x-6 text-sm  sm:mb-0 text-background-on-light dark:text-background-on-dark"
 		>
-			<!-- <li>
+			<li>
 				<a
-					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark"
-					href={base}
+					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark mx-auto"
+					href=""
 				>
-					Privacy Policy
+					<Icon icon="instagram" height="48" width="48" />
 				</a>
 			</li>
 			<li>
 				<a
-					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark"
-					href={base}
+					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark mx-auto"
+					href=""
 				>
-					Licensing
+					<Icon icon="facebook" height="52" width="52" fillColor="" />
 				</a>
 			</li>
-			 -->
+			<li>
+				<a
+					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark mx-auto"
+					href=""
+				>
+					<Icon icon="phone" height="48" width="48" fillColor="DarkSlateBlue" />
+				</a>
+			</li>
+			<li>
+				<a
+					class="inline text-secondary-light dark:text-secondary-dark hover:text-tertiary-light dark:hover:text-tertiary-dark mx-auto"
+					href=""
+				>
+					<Icon icon="email" height="52" width="52" fillColor="FireBrick" />
+				</a>
+			</li>
 		</ul>
 	</div>
 	<hr class="my-6 sm:mx-auto lg:my-8 border-secondary-light/10 dark:border-secondary-dark/10" />
